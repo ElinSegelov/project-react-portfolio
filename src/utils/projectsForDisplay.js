a4803ae7c3eba612ebfbb9cb03ea34bab30e5f92
Elin <elin.segelov@gmail.com>
2023-07-02 19:02:20 +0200

    Update projectsForDisplay.js

--- src/utils/projectsForDisplay.js
+++ src/utils/projectsForDisplay.js
@@ -1,2 +1,2 @@
-export const featuredForDisplay = ['rock-paper-scissor-lizard-spock', 'project-movies', 'final-project', 'space-tourism']
+export const featuredForDisplay = ['rock-paper-scissor-lizard-spock', 'final-project', 'space-tourism', 'project-movies']
 export const otherForDisplay = ['project-todos', 'project-labyrinth', 'project-guess-who', 'project-music-releases', 'project-chatbot', 'project-news-site', 'project-happy-thoughts']
