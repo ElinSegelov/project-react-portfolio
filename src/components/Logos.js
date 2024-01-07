291515957166bc5a8196e15344cf64e704e081cd
Elin <elin.segelov@gmail.com>
2024-01-07 16:52:26 +0100

    Update url for linkedin link

--- src/components/Logos.js
+++ src/components/Logos.js
@@ -4,7 +4,7 @@ import { LogoContainer, StyledLogo } from './ReusableStyles.style';
 const Logos = ({ style, linkedinLogo, githubLogo, section }) => {
   return (
     <LogoContainer style={style}>
-      <a href="https://www.linkedin.com/in/elin-s-683a867a/" aria-label="Link to LinkedIn profile">
+      <a href="https://www.linkedin.com/in/elin-segelov/" aria-label="Link to LinkedIn profile">
         <StyledLogo className={section} src={linkedinLogo} alt="LinkedIn icon" />
       </a>
       <a href="https://github.com/ElinSegelov" aria-label="Link to LinkedIn profile">
